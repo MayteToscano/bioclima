b157dc737cae9c377cc73d43910561f189dd06f9
MayteToscano <maytetd@gmail.com>
2026-05-13 20:00:36 +0200

    Delete scripts directory

--- scripts/generate_bioclimatic_zones.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-"""Generate the bundled demonstration categorical bioclimatic-zone CoverageJSON."""
-print('This repository already includes data/categorical/finland-bioclimatic-zones.covjson. Replace this demonstration generator with an official zonation layer when available.')
